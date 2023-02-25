9d7acd66f7ceb613194e4e008c171395b8d153e6
AlekseevDS <alekseevdms@gmail.com>
2023-02-25 20:40:29 +0100

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const bracketMap = new Map;
+    for (let config of bracketsConfig) {
+        const key = config[0];
+        const value = config[1];
+        bracketMap.set(key, value);
+    }
+    let stackBrackets = [];
+    for (let symbol of str) {
+        if (bracketMap.get(stackBrackets[stackBrackets.length - 1]) === symbol) {
+            stackBrackets.pop()
+        } else {
+            stackBrackets.push(symbol)
+        }
+    }
+    return stackBrackets.length === 0;
 }
